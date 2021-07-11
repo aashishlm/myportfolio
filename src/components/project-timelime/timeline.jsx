047a3f8960444b8049b13c582704e40de6d2a734
aashishlm <giriaashish.lm10@gmail.com>
2021-07-11 16:38:16 +0545

    added cryptosearch app

--- src/components/project-timelime/timeline.jsx
+++ src/components/project-timelime/timeline.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
 
 // projects
+import Currency from "../../assets/img/projects/currency.jpg";
 import Tesla from "../../assets/img/projects/tesla.jpg";
 import Todoapp from "../../assets/img/projects/todoapp.jpg";
 import Netflix_clone from '../../assets/img/projects/Netflix_clone.jpg';
@@ -325,6 +326,98 @@ const TimeLine = () => {
                 </UrlButton>
                 <UrlButton
                   href="https://github.com/aashishlm/tesla-clone"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+                
+                
+              </div>
+            </div>
+          </ImageEvent>
+
+          <ImageEvent
+            date="07/11/2020"
+            className="text-center"
+            text=" Crypto Search"
+            src={Currency}
+            alt="crypto"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This is a Currency Search app where you can search the diffecrent Crypto Currency
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Gives you rate of Cryptocurrency</li>
+                          
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React
+                            </span>
+                          </li>
+                          
+                          
+                          
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://aashishlm.github.io/cryptosearch/"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+                <UrlButton
+                  href="https://github.com/aashishlm/cryptosearch"
                   target="_blank"
                 >
                   SOURCE CODE
